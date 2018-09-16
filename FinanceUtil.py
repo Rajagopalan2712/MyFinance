ced7b66cfc7312442eb341e787fdb278fb01d5f6
Rajagopalan Padmanaban Anand <rajagopalan.padmanaban@citi.com>
2018-09-16 14:22:54 -0500

    Added new method to identify if merchant names partially match

--- FinanceUtil.py
+++ FinanceUtil.py
@@ -6,3 +6,9 @@ def datetime_from_string(input_date: str):
     print(split)
     date_object = date(int(split[2]), int(split[0]), int(split[1]))
     return date_object
+
+
+def isMerchantNamePartialMatch(name1: str, name2: str):
+    if name1 == name2:
+        return False
+    return True
